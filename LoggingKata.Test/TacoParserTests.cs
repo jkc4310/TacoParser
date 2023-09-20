b361a31ed2024337e7c236728504507bbfb1a0ef
kendall chapman <jkc4310@gmail.com>
2023-09-20 14:09:31 -0400

    TacoParser

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -8,7 +8,7 @@ namespace LoggingKata.Test
         [Fact]
         public void ShouldDoSomething()
         {
-            // TODO: Complete Something, if anything
+            // DONE: Complete Something, if anything
 
             //Arrange
             var tacoParser = new TacoParser();
@@ -25,19 +25,37 @@ namespace LoggingKata.Test
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete - "line" represents input data we will Parse to
+            // DONE: Complete - "line" represents input data we will Parse to
             //       extract the Longitude.  Your .csv file will have many of these lines,
             //       each representing a TacoBell location
 
-            //Arrange
+            //Arrange - write the code we need in order to call the method were testing.
+            var tacoParserInstance = new TacoParser();
 
             //Act
+            var actual = tacoParserInstance.Parse(line);
 
             //Assert
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
-        //TODO: Create a test ShouldParseLatitude
+        //DONE: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            //arrange
+            var tacoParserInstance1 = new TacoParser();
+
+            //act
+            var actual = tacoParserInstance1.Parse(line);
+
+            //assert
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
+
+
 
     }
 }
